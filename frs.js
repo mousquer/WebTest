fb040381fcf0725a6d2924e02eac5e3dd869fe31
Raphael Estima Mousquer <31327414+mousquer@users.noreply.github.com>
2025-09-20 17:28:29 -0300

    Delete frs.js
    Arquivo errado.

--- frs.js
+++
@@ -1,5 +0,0 @@
-// Aqui você pode adicionar código JavaScript para interatividade, se necessário.
-// Exemplo: validação de formulário.
-document.querySelector('.contact-form').addEventListener('submit', function(event) {
-    // Código para validar o formulário
-});
